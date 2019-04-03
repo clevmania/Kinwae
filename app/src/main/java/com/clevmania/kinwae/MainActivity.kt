1d8d0e1d9dcf9abb955473fc4685320275afe506
clevmania <ernestcleverest@gmail.com>
2019-04-03 13:54:44 +0100

    populate spinners

--- app/src/main/java/com/clevmania/kinwae/MainActivity.kt
+++ app/src/main/java/com/clevmania/kinwae/MainActivity.kt
@@ -2,7 +2,9 @@ package com.clevmania.kinwae
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_contact.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,10 +12,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        setSupportActionBar(toolbar)
-//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-//        toolbar.setNavigationIcon(R.drawable.ic_back_arrow)
-
         sv_account_opening.go(1,true)
+        populateStateSpinner()
+        populateCitySpinner()
+        handleUpArrow()
+    }
+
+    private fun handleUpArrow(){
+        iv_back_nav.setOnClickListener { finish() }
+    }
+
+    private fun populateStateSpinner(){
+        iv_state_spinner.setOnClickListener { sp_state.performClick() }
+        val stateAdapter = ArrayAdapter
+            .createFromResource(this,R.array.states, android.R.layout.simple_spinner_item)
+        stateAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        sp_state.adapter = stateAdapter
+    }
+
+    private fun populateCitySpinner(){
+        iv_city_spinner.setOnClickListener { sp_city.performClick() }
+        val city = listOf("Ikeja","Maryland","Anthony","Ajah","Victoria Island","Ikoyi")
+        val cityAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, city)
+        cityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        sp_city.adapter = cityAdapter
     }
 }
